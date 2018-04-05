dbd68eefd3ae9cda67d7daa94d8aa2a0870f35b9
Christopher Adeszko <cjadeszko@gmail.com>
2018-04-04 22:16:01 -0700

    Remaining Checkpoints: Complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -57,4 +57,9 @@ class Kele
         puts "Error sending message"
       end
     end
+
+    def get_remaining_checkpoints(chain_id)
+      response = self.class.get("/enrollment_chains/#{chain_id}/checkpoints_remaining_in_section", headers: { "authorization" => @auth_token })
+      JSON.parse(response.body)
+    end
 end
